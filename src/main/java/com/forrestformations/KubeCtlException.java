699fb67c53f706bd662d940e2cba7f6f565e1caa
david.higgins <david.higgins@paconsulting.com>
2025-06-03 11:04:45 +0100

    Change port forward to use kubectl command because remote debug port is not working via client

---
+++ src/main/java/com/forrestformations/KubeCtlException.java
@@ -0,0 +1,7 @@
+package com.forrestformations;
+
+public class KubeCtlException extends RuntimeException {
+    public KubeCtlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
